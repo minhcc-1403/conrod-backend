2dd5e44e435810d5a83d3cf81ca750f2930847a4
minh <minhchiu.official@gmail.com>
2025-03-08 23:20:44 +0700

    feat: applying validation

--- src/domain/users/dto/create-user.dto.ts
+++ src/domain/users/dto/create-user.dto.ts
@@ -1,6 +1,26 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
 export class CreateUserDto {
-  name: string;
-  email: string;
-  phone: string;
-  password: string;
+  @IsString()
+  @Length(2, 50)
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  readonly phone: string;
+
+  @IsString()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+  readonly password: string;
 }
